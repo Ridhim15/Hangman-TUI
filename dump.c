85e2e5dcac4bc93df2d29cd2d2544f23abd825f0
Ridhim <80154613+Ridhim1502@users.noreply.github.com>
2022-10-29 01:06:03 -0700

    Update dump.c

--- dump.c
+++ dump.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include <conio.h>
-void BASIC_SALARY(){
+
+void basic_salary(){
     int sal;
     float da,ra,sum;
     printf("Enter your basic salary\n");
@@ -13,7 +13,7 @@ void BASIC_SALARY(){
     printf("\n");
     sum=sal+da+ra;
     printf("Your gross salary is=%f\n",sum);
-
+    printf("\e[1;1H\e[2J");
 }
 void calc_dist(){
     //(b)distance
@@ -28,22 +28,24 @@ void calc_dist(){
     printf("Distance in centi meters=%f \n",cm);
     printf("Distance in feet=%f \n",f);
     printf("Distance in inches=%f \n",inch);
+    printf("\e[1;1H\e[2J");
 
 }
 void marks(){
     //(c)marks
     unsigned int sub1,sub2,sub3,sub4,sub5;
-    printf("Enter marks of subject 1");
+    printf("Enter marks of subject 1\n");
     scanf("%d",&sub1);
-    printf("Enter marks of subject 2");
+    printf("Enter marks of subject 2\n");
     scanf("%d",&sub2);
-    printf("Enter marks of subject 3");
+    printf("Enter marks of subject 3\n");
     scanf("%d",&sub3);
-    printf("Enter marks of subject 4");
+    printf("Enter marks of subject 4\n");
     scanf("%d",&sub4);
-    printf("Enter marks of subject 5");
+    printf("Enter marks of subject 5\n");
     scanf("%d",&sub5);
     unsigned int b4=(sub1+sub2+sub3+sub4)/400;
+    printf("\e[1;1H\e[2J");
 }
 
 
@@ -52,7 +54,8 @@ void celcius_conversion(){
     printf("Enter the temperature in fahrenheit \n");
     scanf("%f",&f);
     c=(f-32)*5.0/9;
-    printf("Temperature in Celcius is : %f",c);
+    printf("Temperature in Celcius is : %f\n",c);
+    printf("\e[1;1H\e[2J");
 }
 
 void calc_perimeter(){
@@ -73,9 +76,31 @@ void calc_perimeter(){
     printf("Area of the circle is : %f \n",calc);
     calc=2*pi*r;
     prinf("Circumference of the circle is : %f",calc);
+    printf("\e[1;1H\e[2J");
 }
 
-void Paper_size(){
+void paper_size(){
     //(e)paper_size
-    float w,h;
+
+    int w=1189;
+    int h=841;
+    int temp;
+    for (int i=0;i<=8;i++){
+        print("A%d has dimensions : %d x %d",i,w,h);
+        temp=w;
+        w=h;
+        h=temp/2;
+        
+    }
+    printf("\e[1;1H\e[2J");
+}
+
+
+int main(){
+    basic_salary();
+    calc_dist();
+    marks();
+    calc_perimeter();
+    celcius_conversion();
+    paper_size();
 }

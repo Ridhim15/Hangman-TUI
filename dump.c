79f8f3172d740386b7bcd315f1d6f106875e34c1
Ridhim <80154613+Ridhim1502@users.noreply.github.com>
2022-10-29 10:56:02 +0530

    Update dump.c

--- dump.c
+++ dump.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-int main(){
+#include <conio.h>
+void BASIC_SALARY(){
     int sal;
     float da,ra,sum;
     printf("Enter your basic salary\n");
@@ -12,9 +13,9 @@ int main(){
     printf("\n");
     sum=sal+da+ra;
     printf("Your gross salary is=%f\n",sum);
-    
-    
-    
+
+}
+void calc_dist(){
     //(b)distance
     float dist,m,f,inch,cm;
     printf("Enter the distance in Km\n");
@@ -27,7 +28,9 @@ int main(){
     printf("Distance in centi meters=%f \n",cm);
     printf("Distance in feet=%f \n",f);
     printf("Distance in inches=%f \n",inch);
-    
+
+}
+void marks(){
     //(c)marks
     unsigned int sub1,sub2,sub3,sub4,sub5;
     printf("Enter marks of subject 1");
@@ -41,4 +44,38 @@ int main(){
     printf("Enter marks of subject 5");
     scanf("%d",&sub5);
     unsigned int b4=(sub1+sub2+sub3+sub4)/400;
+}
+
+
+void celcius_conversion(){
+    float f,c;
+    printf("Enter the temperature in fahrenheit \n");
+    scanf("%f",&f);
+    c=(f-32)*5.0/9;
+    printf("Temperature in Celcius is : %f",c);
+}
+
+void calc_perimeter(){
+    //(d)perimeter
+    const float pi=3.1415926535897932384626433832795028;
+    float calc,l,b,r;
+    printf("Enter the length of the rectangle \n");
+    scanf("%f",&l);
+    printf("Enter the width of the rectangle \n");
+    scanf("%f",&b);
+    printf("Enter the radius of the circle \n");
+    scanf("%f",&r);
+    calc=l*b;
+    printf("Area of the rectangle is : %f \n",calc);
+    calc=2*(l+b);
+    printf("Perimeter of the rectangle is : %f \n",calc);
+    calc=pi*(r*r);
+    printf("Area of the circle is : %f \n",calc);
+    calc=2*pi*r;
+    prinf("Circumference of the circle is : %f",calc);
+}
+
+void Paper_size(){
+    //(e)paper_size
+    float w,h;
 }
